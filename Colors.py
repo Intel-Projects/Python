b693a03a7f5e80bf425c4f002ac05d07ed4d5f2f
Intel <102398177+Intel-Projects@users.noreply.github.com>
2022-06-01 11:15:25 +0100

    Create Colors.py

---
+++ Colors.py
@@ -0,0 +1,23 @@
+
+class Color:
+    CLN = '\x1b[0m'
+    BOLD = '\x1b[1m'
+    ITALIC = '\x1b[3m'
+
+    BLACK = '\x1b[0;30;40m' # DONE
+    DARK_BLUE = '\x1b[0;34;40m' # DONE
+    DARK_GREEN = '\x1b[0;32;40m' # DONE
+    DARK_AQUA = '\u001b[38;5;30m' # DONE
+    DARK_RED = '\x1b[5;31;40m' # DONE
+    DARK_PURPLE = '\x1b[4;35;40m' # DONE
+    GOLD = '\x1b[5;33;40m' # DONE
+    GRAY = '\x1b[1;30;40m' # DONE
+    DARK_GRAY = '\u001b[38;5;241m' # DONE
+    BLUE = '\x1b[1;34;40m' # DONE
+    GREEN = '\x1b[1;32;40m' # DONE
+    AQUA = '\x1b[1;36;40m' # DONE
+    RED = '\x1b[1;31;40m' # DONE
+    LIGHT_PURPLE = '\x1b[200m'
+    YELLOW = '\u001b[33m'
+    WHITE = '\x1b[7;37;40m'
+    RESET = '\u001b[0m'
